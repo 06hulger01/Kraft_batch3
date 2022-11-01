8a09adf7832881dbf3b0ab37850ac1a2af4d8e29
isikharun <isikharun@outlook.com>
2022-11-02 01:45:51 +0300

    day 2

---
+++ src/day_1_MyFirstProgram/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package day_1_MyFirstProgram;
+
+public class MyFirstProgram {
+    public static void main( String[] args ){
+        System.out.println("Merhaba dünya");
+        System.out.println();
+        System.out.print("bugün hava çok güzel");
+    }
+//OrnekBir adında yeni bir class oluşturun ve
+// aşağıdaki yazıyı konsola yazdırın.
+// (imlecin yerine dikkat)
+}
---
+++ src/day_1_MyFirstProgram/Ornek_iki.java
@@ -0,0 +1,16 @@
+package day_1_MyFirstProgram;
+
+public class Ornek_iki {
+    public static void main(String[] args){
+        System.out.println("Bilginin");
+        System.out.println("efendisi");
+        System.out.println("olmak için");
+        System.out.println();
+        System.out.println("çalışmanın");
+        System.out.println("kölesi");
+        System.out.println();
+        System.out.println();
+        System.out.println("olmak gerekir.");
+
+    }
+}
