d8a393fc9f9d6010b92afa5da548e3844c59a1d9
salih-salih <salihyilmazoglu@hotmail.com>
2022-11-03 22:09:41 +0300

    day3 update and day4 added

--- src/day_3_dataTypes/DataTypes.java
+++ src/day_3_dataTypes/DataTypes.java
@@ -1,4 +1,4 @@
-package day_3_DataTypes;
+package day_3_dataTypes;
 
 public class DataTypes {
     /*
@@ -35,7 +35,7 @@ public class DataTypes {
         //initialize or assigning
         number1 = 10;
         System.out.println(number1);
-        sayMyName = "Adnan";
+        sayMyName = "Adnan ";
 //        System.out.println(sayMyName);
 
         //Declaration and assigning
@@ -43,6 +43,7 @@ public class DataTypes {
         System.out.println(number2);
         System.out.println(number1+number2);// 30---> 36. satırdan gelen number1 ile number2 nin toplamı
         String sayMySurname = "Buyuk";
+        System.out.println(sayMyName+sayMySurname);
 
         System.out.println("--------------");
         int i1,i2;
@@ -82,12 +83,54 @@ public class DataTypes {
         // tipinde bir değişkene declare edileceiğini belirtmemizi isiyor
         System.out.println(l);
         l=348955459;//burada ise int sınırlarını aşmadığı için hata vermiyor
-
         System.out.println(l);
-
         System.out.println("---------------");
 
+        //Floating point number // decimal numbers default to double//
+        //float and double
+        //expressing currency// banka hesaplarının gosteriminde kullnılan tipler
+        //float 4 byte | double 8 byte
+        f =1.12345671234567F;//4 byte
+        d =1.123456712344;//8 byte
 
+        System.out.println("Hacı = "+f);
+        System.out.println("d = " + d);
+        System.out.println("-------------------");
+        //Char
+        //https://unicode-table.com/en/#basic-latin
+        char ch = 'A';
+        System.out.println(ch);//a
+
+        int ascii = ch;
+        System.out.println("ascii = " + ascii);
+//        int x = 65;
+//        char chA =(char) x;
+
+        char chA = 65;
+        System.out.println("chA = " + chA);//A
+        char cha = 97;
+        System.out.println("cha = " + cha);//a
+        char chZ = 90;
+        System.out.println("chZ = " + chZ);//Z
+        char chz = 122;
+        System.out.println("chz = " + chz);//z
+
+        String A = "\u0041";
+        System.out.println("A = " + A);
+
+        //boolean ----true yada false
+        bl=true;
+        bl=false;
+        System.out.println("bl = " + bl);
+
+        int a = 10;
+        int t = 10;
+        boolean bln = a==t;
+        System.out.println("bln = " + bln);//true
+
+        int z = 30;
+        bln = z == a+t;
+        System.out.println("bln = " + bln);//flase
 
     }
 
