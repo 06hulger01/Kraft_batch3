08cffbc3d7fcaf1719840872b045293f7368cf08
salih-salih <salihyilmazoglu@hotmail.com>
2022-11-04 22:28:22 +0300

    day5 added

---
+++ src/day_5_review/Binary.java
@@ -0,0 +1,57 @@
+package day_5_review;
+
+public class Binary {
+    public static void main(String[] args){
+        /*
+        >java fundamentals coming from c++//javanın temeli c++ dilinden gelmekte
+        >java simple learn/secure/high level/high performance/dynamic.....programing language
+        >java known "platform independent programing language"---write anywhere run everywhere
+                        //herhangi bir platformda yaz istediğin platformda run et.
+               JAVA COMPONENTS(javanın bölümleri)
+               -------------------
+               |    JDK          |--->java development kit
+               |    -------------|
+               |    |    jRE     |--->java runtime environment
+               |    |   ---------|
+               |    |   |  JVM   |--->java virtual machine provide platform independence
+               |    |   |        |
+               -------------------
+
+        */
+
+        //binary
+
+        //windows+R---charmap---özel karakter tablosu açılır
+        //Mac için language & text penceresi altında character viewer
+        //how to open character viewer on Mac
+
+        //0 üstlü sayısı için u+2070
+        //1 üstlü sayısı için ALT+0185
+        //2 üstlü sayısı için ALT+0178
+        //3 üstlü sayısı için ALT+0179
+        //4 üstlü sayısı için u+2074
+        //5 üstlü sayısı için u+2075
+        //6 üstlü sayısı için u+2076
+        //7 üstlü sayısı için u+2077
+        //8 üstlü sayısı için u+2078
+        //9 üstlü sayısı için u+2079
+
+        //54
+        //54-32=22
+        //22-16=6
+        //6-4=2
+        //2-2=0
+
+        //2⁵    2⁴      2³      2²      2¹      2⁰
+        //32    16      8       4       2       1               //convert decimal to binary
+        //1     1       0       1       1       0
+
+
+        //1     0       1       0       1       0
+        //32    16      8       4       2       1               //convert binary to decimal
+        //32  + 0   +   8   +   0    +  2   +   0   = 42
+
+
+        //https://www.rapidtables.com/convert/number/binary-to-decimal.html
+    }
+}
