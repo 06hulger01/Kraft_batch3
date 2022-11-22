a2b797113ebe4be51d224d58537917d285b97ced
isikharun <isikharun@outlook.com>
2022-11-22 22:05:26 +0300

    day 17

---
+++ src/day_17_Methods/Methods_Cont.java
@@ -0,0 +1,47 @@
+package day_17_Methods;
+
+import java.util.Scanner;
+
+public class Methods_Cont {
+
+    //kullanıcında 2 adet sayı alan ve bu sayıların ortalamasını veren
+    //ortalama metodunu yazalım
+    public static void ortalama(){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("sayi1: ");
+        int x= scan.nextInt();
+        System.out.print("sayi2: ");
+        int y= scan.nextInt();
+        int ortalama1=(x+y)/2;
+        System.out.println("Ortalama: "+ortalama1);
+    }
+    //kullanıcında 2 adet sayı alan en büyük sayıyı ekrana yazdırın
+
+    public static void main(String[] args) {
+        enBuyuk();
+
+
+    }
+
+
+    public static void enBuyuk(){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("sayi1: ");
+        int x= scan.nextInt();
+        System.out.print("sayi2: ");
+        int y= scan.nextInt();
+        int result;
+       /* if(x>=y){
+            result=x;
+        }
+        else{
+            result=y;
+        }
+*/
+        result=(x>=y)?x:y;
+        System.out.println("enbuyuk sayi "+result);
+    }
+
+
+
+}
--- src/day_1_MyFirstProgram/MyFirstProgram.java
+++ src/day_1_MyFirstProgram/MyFirstProgram.java
@@ -1,10 +1,13 @@
 package day_1_MyFirstProgram;
 
+import day_17_Methods.Methods;
+
 public class MyFirstProgram {
     public static void main( String[] args ){
-        System.out.println("Merhaba dünya");
+      System.out.println("Merhaba dünya");
         System.out.println();
         System.out.print("bugün hava çok güzel");
+
     }
 //OrnekBir adında yeni bir class oluşturun ve
 // aşağıdaki yazıyı konsola yazdırın.
