3303927bc1038e60069112f200b53febf4e062f1
isikharun <isikharun@outlook.com>
2022-11-28 21:19:07 +0300

    day 21

--- src/day_1_MyFirstProgram/MyFirstProgram.java
+++ src/day_1_MyFirstProgram/MyFirstProgram.java
@@ -1,10 +1,12 @@
 package day_1_MyFirstProgram;
 
 import day_17_Methods.Methods;
+import day_21_Methods_Summary_And_Class_Object.Methods_Summary;
 
 public class MyFirstProgram {
     public static void main( String[] args ){
-      System.out.println("Merhaba dünya");
+
+        System.out.println("Merhaba dünya");
         System.out.println();
         System.out.print("bugün hava çok güzel");
 
