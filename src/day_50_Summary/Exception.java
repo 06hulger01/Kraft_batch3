5c1c6ab7f48181bbe38c8596f7608de51534b0d0
isikharun <isikharun@outlook.com>
2023-01-06 21:11:58 +0300

    day 50 cont 2

--- src/day_50_Summary/Exception.java
+++ src/day_50_Summary/Exception.java
@@ -4,32 +4,49 @@ import java.util.Scanner;
 
 public class Exception {
     public static void main(String[] args) {
-/*
+
         System.out.println("Program başladı");
         try {
             int x=5;
-            int y=0;
+            int y=1;
             int z=x/y;
             System.out.println(z);
             System.out.println("----------sorun yok aferim böyle devam et-----");
         }catch (java.lang.Exception e){
             sum();
             System.out.println("Hatalı işlem ama programa devam ediyorum bir dahakinde yanlış yapma");
+        }finally {
+            System.out.println("finally bloğu çalıştı");
         }
 
         System.out.println("program devam ediyor");
 
         System.out.println("Merhaba Hoş geldiniz");
-     */
 
+/*
         System.out.println("Sistemize hoş geldiniz");
         int parola=123;
 
-        for (int i = 0; i <3 ; i++) {
 
+
+        for (int i = 0; i <3 ; i++) {
             Scanner scan=new Scanner(System.in);
             System.out.print("Parola : ");
-            int parola2= scan.nextInt();
+
+            int parola2=0;
+
+            try {
+
+                 parola2=scan.nextInt();
+            }catch (java.lang.Exception e){
+                String x=e.toString();
+               String [] arr=x.split("\\.");
+               System.out.println(arr[2]);
+              //  System.out.println(x);
+                System.out.println("Lütfen sayısal bir değer giriniz");
+                i--;
+               // continue;
+            }
             if (parola2==parola) {
                 System.out.println("Giriş yapıldı");
                break;
@@ -49,7 +66,7 @@ public class Exception {
 
 
         System.out.println();
-
+*/
 
 
 
