67899ac48de46de8b5945ed178cca73f3f0d5a64
isikharun <isikharun@outlook.com>
2023-01-10 20:01:34 +0300

    day 52 contain

--- src/day_52_Collection_Set/List_Polindrom.java
+++ src/day_52_Collection_Set/List_Polindrom.java
@@ -9,13 +9,46 @@ public class List_Polindrom {
         // eşit olan kelimeler) kelimeleri ekrana yazdıran metodu yazınız.
         //kaşık,adanada,kazak,radar,ses,harun
         //adanada,kazak,radar,ses
-        List<String> list=Arrays.asList("kaşık","adanada","kazak","radar","ses","harun");
-        polindromKelimeBul1(list);
+
         //list içerisinde bulunan polindrom (terside kendisine
         // eşit olan kelimeler) kelimeleri ekrana yazdıran metodu yazınız.(aynı kelimeleri ekrana yazma-sın)
 
 
+        List<String> list=Arrays.asList("kaşık","adanada","kazak","radar","ses","harun","ses");
+        polindromKelimeBul4(list);
 
+    }
+
+//removeAll metodunu kullanın containns kullanmayın 15 dk ara
+    public static void polindromKelimeBul5(List<String> list){
+
+        for (int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            String reverse = "";
+            for (int j = str.length()-1; j >=0;  j--) {
+                reverse+=str.charAt(j);
+            }
+            if(str.equalsIgnoreCase(reverse)){
+                System.out.println(str+" ");
+                //removeAll-remove ikisinden biri
+            }
+        }
+
+    }
+
+    public static void polindromKelimeBul4(List<String> list){
+        List<String> newList=new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            String reverse = "";
+            for (int j = str.length()-1; j >=0;  j--) {
+                reverse+=str.charAt(j);
+            }
+            if(str.equalsIgnoreCase(reverse) && !newList.contains(str)){
+                newList.add(str);
+                System.out.println(str+" ");
+            }
+        }
 
     }
 
